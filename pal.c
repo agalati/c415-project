28b41ea50c12d6ce376654b1308d2f8bfd26b91d
Matthew Low <lithravant@warpmail.net>
2011-10-07 21:39:03 -0400

    Fixed program listing to output all errors for the given line

--- pal.c
+++ pal.c
@@ -151,10 +151,11 @@ new_position_line(void)
     fprintf(lst_file, "%s\n", linebuf);
     free(linebuf);
     linebuf = pop_err_from_buf();
-    if (linebuf)
+    while (linebuf)
     {
       fprintf(lst_file, "%s", linebuf);
       free(linebuf);
+      linebuf = pop_err_from_buf();
     }
   }
   yylloc.first_column = 1;
