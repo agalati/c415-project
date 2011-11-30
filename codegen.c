57e19be651c3e79ddfdbd0bc3534f95243e8d8ef
Matthew Low <mlow@ualberta.ca>
2011-11-29 17:02:57 -0700

    Started code generation

---
+++ codegen.c
@@ -0,0 +1,13 @@
+/*
+ * codegen.h
+ *
+ * Authors
+ *   - Matthew Low
+ *   - Anthony Galati
+ *   - Mike Bujold
+ *   - Stevan Clement
+ */
+
+#ifndef CODEGEN_H
+#define CODEGEN_H
+#include "codegen.h"
---
+++ codegen.h
@@ -0,0 +1,17 @@
+/*
+ * codegen.h
+ *
+ * Authors
+ *   - Matthew Low
+ *   - Anthony Galati
+ *   - Mike Bujold
+ *   - Stevan Clement
+ */
+
+#ifndef CODEGEN_H
+#define CODEGEN_H
+
+void emit_iconst(integer val);
+void emir_rconst(integer val);
+
+#endif
