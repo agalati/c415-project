ff9e0f9dde3f5c7e82ea0785fb6c69672e89d832
Anthony Galati <agalati@ualberta.ca>
2011-12-04 03:05:03 -0700

    Added a diagram of the symbol table, put together some formal references

---
+++ documentation/changes.c
@@ -0,0 +1,46 @@
+/*
+ *		Changes to Documentation
+ * 
+ * 	Some things to add for the final submission
+ * */
+
+	#more detailed references:
+		-add a few more references
+		-stick in some formal citations
+	
+	"Bison - GNU Parser Generator." The GNU Operating System. Free Software Foundation (FSF). 
+		Web. 04 Dec. 2011. <http://www.gnu.org/software/bison/manual/>.
+		
+	Donnelly, Charles, and Richard Stallman. "Bison, The YACC-compatible Parser Generator." 
+		The LEX & YACC Page. Web. 04 Dec. 2011. <http://dinosaur.compilertools.net/bison/index.html>.
+		
+	Paxson, Vern. "Flex - a Scanner Generator." The LEX & YACC Page. 
+		Web. 04 Dec. 2011. <http://dinosaur.compilertools.net/flex/index.html>.
+		
+	Levine, John. "Flex & Bison." Google Books. O'Reilly Media, Inc. 
+		Web. 04 Dec. 2011. <http://books.google.ca/books?id=3Sr1V5J9_qMC>.
+		
+	Rudnicki, Piotr. "CMPUT 415 Class Notes." In-Class. First accessed 09 Sept. 2011. 
+	
+	#problems encountered at each CP:
+		CP1:
+			-issues with grammar
+			-issues with regex to each comments, etc 
+			-error reporting and recovery
+			
+		CP2:
+			-issues with sym table
+			-resolving some older problems
+			-issues with grammar affecting array instantiation
+			-error recovery, especially with unterminated strings and missing ;
+	
+	
+	#more details on symbol table:
+		-more verbose
+		-throw in some diagrams of shiz
+		
+	
+	
+	
+  
+  
